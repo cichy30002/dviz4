0b5f0d21b4bb9bf7ad7ec14160071b6b4d7249ae
mateugazi <mateugazi@protonmail.com>
2022-06-04 15:27:50 +0200

    Added datatable and added ui

--- app/server.R
+++ app/server.R
@@ -10,57 +10,64 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
+library(DT)
 
 data = read.csv("AB_NYC_2019.csv",nrows = 1000)
 unique(data$neighbourhood_group)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- dplyr::filter(data,data$price %in% (input$priceRangeHist[1]:input$priceRangeHist[2]))$price
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-    output$mapPlotPrice <- renderLeaflet({
-      x <- dplyr::filter(data,data$price %in% (input$priceRangeMap[1]:input$priceRangeMap[2]))
-      
-      leaflet(data = x) %>% addTiles() %>%  addMarkers(~longitude, ~latitude, label = paste("Name:", x$name),clusterOptions = markerClusterOptions())
-        
-    })
-    output$mapPlotType <- renderLeaflet({
-      pal <- colorFactor(c("blue", "red", "green"), domain = unique(data$room_type))
-      x <- dplyr::filter(data,data$neighbourhood_group %in% input$roomTypeNeighbourhoodGroupMap)
-      
-      leaflet(data = x) %>% addTiles() %>% addCircleMarkers(~longitude, ~latitude, radius = 1, opacity = 0.3, fillOpacity = 0.1,
-                                                            color = ~pal(room_type), label = paste("Name:", x$name)) %>%
-        addLegend(pal=pal, values = ~room_type, title = "Room types", opacity=0.8)
-    })
-    output$mapPlotNeighbourhood <- renderLeaflet({
-      pal <- colorFactor(topo.colors(length(unique(data$neighbourhood))), domain = unique(data$neighbourhood))
-      
-      map = leaflet(data = data) %>% addTiles() %>% addCircleMarkers(~longitude, ~latitude, radius = 1, opacity = 0.3, fillOpacity = 0.1,
-                                                            color = ~pal(neighbourhood), label = paste("neighbourhood:", data$neighbourhood))
+  
+  output$distPlot <- renderPlot({
     
-    })
-    output$availabilityPlot <- renderPlot({
-      
-      # generate bins based on input$bins from ui.R
-      bounds = input$mapPlotPrice_bounds
-      
-      x    <- dplyr::filter(data, between(data$longitude, bounds$west, bounds$east) & between(data$latitude, bounds$south, bounds$north))$availability_365
-      if(length(x) == 0){return()}else{
+    # generate bins based on input$bins from ui.R
+    x    <- dplyr::filter(data,data$price %in% (input$priceRangeHist[1]:input$priceRangeHist[2]))$price
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  output$mapPlotPrice <- renderLeaflet({
+    x <- dplyr::filter(data,data$price %in% (input$priceRangeMap[1]:input$priceRangeMap[2]))
+    
+    leaflet(data = x) %>% addTiles() %>%  addMarkers(~longitude, ~latitude, label = paste("Name:", x$name),clusterOptions = markerClusterOptions())
+    
+  })
+  output$mapPlotType <- renderLeaflet({
+    pal <- colorFactor(c("blue", "red", "green"), domain = unique(data$room_type))
+    x <- dplyr::filter(data,data$neighbourhood_group %in% input$roomTypeNeighbourhoodGroupMap)
+    
+    leaflet(data = x) %>% addTiles() %>% addCircleMarkers(~longitude, ~latitude, radius = 1, opacity = 0.3, fillOpacity = 0.1,
+                                                          color = ~pal(room_type), label = paste("Name:", x$name)) %>%
+      addLegend(pal=pal, values = ~room_type, title = "Room types", opacity=0.8)
+  })
+  output$mapPlotNeighbourhood <- renderLeaflet({
+    pal <- colorFactor(topo.colors(length(unique(data$neighbourhood))), domain = unique(data$neighbourhood))
+    
+    map = leaflet(data = data) %>% addTiles() %>% addCircleMarkers(~longitude, ~latitude, radius = 1, opacity = 0.3, fillOpacity = 0.1,
+                                                                   color = ~pal(neighbourhood), label = paste("neighbourhood:", data$neighbourhood))
+    
+  })
+  output$availabilityPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    bounds = input$mapPlotPrice_bounds
+    
+    x    <- dplyr::filter(data, between(data$longitude, bounds$west, bounds$east) & between(data$latitude, bounds$south, bounds$north))$availability_365
+    if(length(x) == 0){return()}else{
       bins <- seq(min(x), max(x), length.out = input$binsAvailability + 1)
       
       # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
       
     }})
-    observeEvent(input$mapPlotNeighbourhood_bounds, {
-      print(input$mapPlotNeighbourhood_bounds)
-    })
+  observeEvent(input$mapPlotNeighbourhood_bounds, {
+    print(input$mapPlotNeighbourhood_bounds)
+  })
+  
+  output$nycDatatable = DT::renderDataTable({
+    data
+  }, options = list(
+    scrollX = TRUE
+  ))
 })
--- app/ui.R
+++ app/ui.R
@@ -5,52 +5,156 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+# 
 
 library(shiny)
-
+library(shinydashboard)
+library(leaflet)
+library(DT)
 data = read.csv("AB_NYC_2019.csv")
+
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            sliderInput("priceRangeHist",
-                         "Price range for histogram:",
-                        min = 0,
-                        max = max(data$price),
-                        value = c(0,max(data$price))),
-            sliderInput("priceRangeMap",
-                        "Price range for map:",
-                        min = 0,
-                        max = max(data$price),
-                        value = c(300,max(data$price))),
-            checkboxGroupInput("roomTypeNeighbourhoodGroupMap", "Neighbourhood group",
-                               unique(data$neighbourhood_group), selected = unique(data$neighbourhood_group)),
-            sliderInput("binsAvailability",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot"),
-            leafletOutput("mapPlotPrice"),
-            leafletOutput("mapPlotType"),
-            leafletOutput("mapPlotNeighbourhood"),
-            plotOutput("availabilityPlot")
-        )
-        
+dashboardPage(
+#  dashboardHeader(title = div(span("NYC Airbnb"), img(src="logo.png", class="airbnb-logo"))),
+  dashboardHeader(title = tags$a(href='', class="airbnb-logo-container",
+                                 tags$img(src="white-logo.png", height = '35', width = '35'),
+                                'NYC Airbnb', target="_blank", class="airbnb-logo")),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(span("Dashboard"), tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Maps", tabName = "maps", icon = icon("map")),
+      menuItem("About", tabName = "about", icon = icon("info"))
     )
-))
+  ),
+  dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+    ),
+    
+    tabItems(
+      tabItem(tabName = "dashboard",
+              h2("Dashboard", id="tab-title"),
+              box(
+                title = "Co to w ogóle jest? xD nazwij to jakoś proszę", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                plotOutput("distPlot"),
+              ),
+              box(
+                title = "Inputs for to coś", status = "warning", solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                sliderInput("bins",
+                            "Number of bins:",
+                            min = 1,
+                            max = 50,
+                            value = 30),
+              ),
+              box(
+                title = "Datatable",
+                status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                DT::dataTableOutput("nycDatatable"),
+                width = 12
+              )
+              
+      ),
+      
+      tabItem(tabName = "maps",
+              h2("Maps", id="tab-title"),
+              box(
+                title = "Histogram", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                leafletOutput("mapPlotPrice"),
+              ),
+              
+              box(
+                title = "Inputs", status = "warning", solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                sliderInput("priceRangeHist",
+                            "Price range for histogram:",
+                            min = 0,
+                            max = max(data$price),
+                            value = c(0,max(data$price))),
+              ),
+              
+              box(
+                title = "Map of places", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                leafletOutput("mapPlotType"),
+              ),
+              
+              box(
+                title = "Inputs", status = "warning", solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                sliderInput("priceRangeMap",
+                            "Price range for map:",
+                            min = 0,
+                            max = max(data$price),
+                            value = c(300,max(data$price))),
+              ),
+              
+              box(
+                title = "Histogram", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                leafletOutput("mapPlotNeighbourhood")
+              ),
+              
+              box(
+                title = "Inputs", status = "warning", solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                checkboxGroupInput("roomTypeNeighbourhoodGroupMap", "Neighbourhood group",
+                                   unique(data$neighbourhood_group), selected = unique(data$neighbourhood_group))
+              ),
+              
+              box(
+                title = "Histogram of prices on the current region of the map", status = "primary", solidHeader = TRUE,
+                collapsible = TRUE,
+                plotOutput("availabilityPlot")
+              ),
+              
+              box(
+                title = "Inputs", status = "warning", solidHeader = TRUE,
+                "Box content here", br(), "More box content",
+                sliderInput("binsAvailability",
+                            "Number of bins:",
+                            min = 1,
+                            max = 50,
+                            value = 30)
+              )
+      ),
+      
+      tabItem(tabName = "about",
+              h2("About", id="tab-title"),
+              div(
+                p(
+                  "Around the turn of the year 2019/20 there was a coronavirus outbreak, which shocked the world.
+                  Nearly everyone on the planet was somehow affected by it and for travel industry it was no exception.
+                  The booking number plummeted due to the governments restriction on movement and people's panic of getting infected.
+                  However, now the situation seems to be stabilazing thanks to the vaccines.
+                  "
+                ),
+                p(
+                  "Due to that fact the number of hotel bookings is constantly growing, but still it is still below 2019 values.
+                  That's why we decided to create an application for visualizing accomodation listing in given area.
+                  We decided to choose New York City, because it is one of the most important cities in the world and we also
+                  found many proves for increased booking demand there.
+                  We also discovered excellent dataset with nearly 50 thousand listings from one of the most popular platform for booking Airbnb.
+                  This data is from pre-pandemic year of 2019 when there was many options available."
+                ),
+                p(
+                  "We believe that our app can help with getting insigts about the market there for example, which neighbourhoods were the most popular ones."
+                ),
+                p (
+                  "Links: ",
+                  br(),
+                  a("Dataset", href="https://www.kaggle.com/datasets/dgomonov/new-york-city-airbnb-open-data",
+                    target="_blank"),
+                  br(),
+                  a("Example tourism forecast", href="https://www.cityguideny.com/article/forecast-nyc-2022-tourism#:~:text=NYC%20%26%20Co.%20is%20anticipating%2056.4,70%25%20increase%20from%20last%20year.",
+                    target="_blank")
+                ),
+                class="about-text")
+      )
+    )
+  )
+)
+
